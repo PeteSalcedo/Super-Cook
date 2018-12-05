05e73d1bed309258459993967eeb45c920ae6eb5
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-04 19:08:39 -0600

    Uploading updated get method titles rendering to screen

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 
 import Form from "./components/Form.js"
 
-const API_KEY = "your api key";
+const API_KEY = "Your Api key here";
 
 
 class App extends Component {
+
+  state = {
+   recipes: []
+
+  }
+
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
 
     const data = await api_call.json();
-    console.log(data)
+      this.setState({recipes: data.recipes })
+      console.log(this.state.recipes);
   }
   render() {
     return (
@@ -22,6 +29,9 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} /> 
+        { this.state.recipes.map((recipe) => {
+          return <p key= {recipe.recipe_id}>{recipe.title}</p>
+        })}
       </div>
     );
   }
